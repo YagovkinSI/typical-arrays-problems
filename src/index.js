3388eb6d078d60279fcf8d501d60d2d786000095
Сергей Яговкин <scorpionupgrade@mail.ru>
2020-03-14 17:56:25 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!Array.isArray(array) || array.length == 0)
+        return 0;
+    var min = Number.MAX_VALUE;
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] < min)
+            min = array[i];
+    }
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!Array.isArray(array) || array.length == 0)
+        return 0;
+    var max = Number.MIN_VALUE;
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] > max)
+            max = array[i];
+    }
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!Array.isArray(array) || array.length == 0)
+        return 0;
+    var sum = 0;
+    var count = 0;
+    for (var i = 0; i < array.length; i++) {
+        sum += array[i];
+        count++;
+    }
+    return sum / count;
 }
